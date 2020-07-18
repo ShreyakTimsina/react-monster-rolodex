e7dbe92dcdc2a90e789b49edb0a7917734ff466a
Shreyak Timsina <Shreyak>
2020-07-18 15:44:00 +0545

    Updates

---
+++ precache-manifest.b3d600dc81711f5b55ea288f96eb01ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec61a0cb41a95868f8560300a18cf6be",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "ab980ef484673b1bc745",
+    "url": "/react-monster-rolodex/static/css/main.f1f8e041.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab980ef484673b1bc745",
+    "url": "/react-monster-rolodex/static/js/main.98113fed.chunk.js"
+  },
+  {
+    "revision": "e5eb423b2a2cc1d9f46f",
+    "url": "/react-monster-rolodex/static/js/runtime-main.856af338.js"
+  }
+]);
